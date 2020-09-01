0b5080d878a1fa8de377f9f5d0cdd0ad054a7c32
Daniel Gatis <danielgatis@gmail.com>
2020-09-01 13:09:19 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="docscan",
-    version="1.0.2",
+    version="1.0.3",
     description="This is a document scanner",
     long_description=long_description,
     long_description_content_type="text/markdown",
